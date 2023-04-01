0e086d65ad69891870dcc28a829afc0c4ba492b8
DAHUO-Melbourne <huodamelbourne@gmail.com>
2023-04-01 23:37:23 +1100

    4-2

--- components/Login/index.tsx
+++ components/Login/index.tsx
@@ -1,14 +1,70 @@
 import { NextPage } from 'next';
-import React from 'react';
+import React, { useState } from 'react';
+import styles from './index.module.scss';
 
 interface LoginProps {
   isShow: boolean;
   onClose: () => void;
 }
 
-const Login: NextPage<LoginProps> = ({ isShow }: LoginProps) => {
-  console.log(isShow);
-  return <div>index</div>;
+const Login: NextPage<LoginProps> = ({
+  isShow = false,
+  onClose,
+}: LoginProps) => {
+  const [form, setForm] = useState({
+    phone: '',
+    verify: '',
+  });
+  const handleGetVerifyCode = () => {};
+
+  const handleOAuthGithub = () => {};
+
+  const handleLogin = () => {};
+  return isShow ? (
+    <div className={styles.loginArea}>
+      <div className={styles.loginBox}>
+        <div className={styles.loginTitle}>
+          <div>phone num login</div>
+          <div className={styles.close} onClick={onClose}>
+            x
+          </div>
+        </div>
+        <input
+          name="phone"
+          type="text"
+          placeholder="please input phone number"
+          value={form.phone}
+        />
+        <div className={styles.verifyCodeArea}>
+          <input
+            name="verify"
+            type="text"
+            placeholder="please input verify code"
+            value={form.verify}
+          />
+          <span className={styles.verifyCode} onClick={handleGetVerifyCode}>
+            get verify code
+          </span>
+        </div>
+        <div className={styles.loginBtn} onClick={handleLogin}>
+          login
+        </div>
+        <div className={styles.otherLogin} onClick={handleOAuthGithub}>
+          Use Github Login
+        </div>
+        <div className={styles.loginPrivacy}>
+          register/login means agree with
+          <a
+            href="https://moco.imooc.com/privacy.html"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Privacy policy
+          </a>
+        </div>
+      </div>
+    </div>
+  ) : null;
 };
 
 export default Login;
